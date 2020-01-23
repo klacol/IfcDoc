fc25c2a5424a0177a1ab023c936f6c33329abf08
Klaus Aengenvoort <klaus.aengenvoort@etask.de>
2020-01-23 20:18:40 +0100

    Change all *htm extensions to *.html to be able, to render the docs in Github-Pages

--- DocumentationISO.cs
+++ DocumentationISO.cs
@@ -4992,13 +4992,43 @@ namespace IfcDoc
 				using (FormatHTM htmSection = new FormatHTM(path + "\\cover.html", mapEntity, mapSchema, included))
 				{
 					htmSection.WriteHeader(docPublication.Name, 0, docPublication.Header);
-					htmSection.Write(
-						"\r\n" +
-						"<script type=\"text/javascript\">\r\n" +
-						"<!--\r\n" +
-						"    parent.index.location.replace(\"credits.html\");\r\n" +
-						"//-->\r\n" +
-						"</script>\r\n");
+                    htmSection.Write(
+                     $@"<script type=""text/javascript"">
+                        <!--
+                            parent.index.location.replace(""credits.html"");
+                        //-->
+                        </script>
+                        <div style=""margin-top: 50px; align: center"">
+                          <p>&nbsp;</p>
+                          <table style=""margin-left: auto; margin-right: auto; background-repeat: no-repeat; background-image: url(img/concerthall.png); width: 740px; padding: 20px"">
+                            <tbody><tr style=""height: 90px"">
+                              <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                              <td>&nbsp;</td>
+                            </tr>
+                            <tr style=""height: 320px"">
+                              <td style=""vertical-align: top"">
+                                <p style=""text-align:left;"">
+                                  <span style=""font-family:Arial, Verdana, Tahoma, sans-serif; font-size:xx-large; color:#FFFFFF; "">
+                                    <b>{docPublication.Name}</b></span><br><br><br>
+                                  <span style=""font-family:Arial, Verdana, Tahoma, sans-serif; font-size:x-large; color:#FFFFFF; "">
+                                    <b>Version {docPublication.Version}</b></span> 
+                                </p>
+                              </td>
+                            </tr>
+                            <tr style=""height: 80px"">
+                              <td style=""vertical-align: top;"">
+                                <p style=""text-align:left;"">
+                                  <span style=""font-family:Arial, Verdana, Tahoma, sans-serif; font-size:small; "">
+                                    © buildingSMART 1996-{DateTime.Now.Year} - This document is owned and copyrighted by {docPublication.Owner}<br>
+                                    By using the IFC4 specification you agree to the following <a href=""copyright.html"" target=""info""><b>copyright notice</b></a></span>
+                                </p>
+                              </td>
+                            </tr>
+                          </tbody></table>
+                        </div>"
+                    );
 					htmSection.WriteLine(docPublication.DocumentationHtml());
 					htmSection.WriteFooter(docPublication.Footer);
 				}
